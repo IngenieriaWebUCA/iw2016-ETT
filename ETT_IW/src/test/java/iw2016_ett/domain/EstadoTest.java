117ca2c9d9271b1ecb6a71d56af85b8d88b4863a
k15kor <thek15@Macintosh.local>
2016-05-31 19:08:52 +0200

    Test unitarios  mock
    test mock --entity ~.domain.nombreEntidad

---
+++ ETT_IW/src/test/java/iw2016_ett/domain/EstadoTest.java
@@ -0,0 +1,14 @@
+package iw2016_ett.domain;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.springframework.mock.staticmock.MockStaticEntityMethods;
+
+@RunWith(JUnit4.class)
+@MockStaticEntityMethods
+public class EstadoTest {
+
+    @Test
+    public void testMethod() {
+    }
+}
---
+++ ETT_IW/src/test/java/iw2016_ett/domain/PerfilTest.java
@@ -0,0 +1,14 @@
+package iw2016_ett.domain;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.springframework.mock.staticmock.MockStaticEntityMethods;
+
+@RunWith(JUnit4.class)
+@MockStaticEntityMethods
+public class PerfilTest {
+
+    @Test
+    public void testMethod() {
+    }
+}
