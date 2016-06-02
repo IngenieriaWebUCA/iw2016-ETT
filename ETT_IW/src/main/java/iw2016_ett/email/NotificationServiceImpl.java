6518bd726f5fc1c7129206da6524915ed8e3c432
juang7 <juangarpe94@gmail.com>
2016-06-02 13:43:19 +0200

    correo2.0

--- ETT_IW/src/main/java/iw2016_ett/email/NotificationServiceImpl.java
+++ ETT_IW/src/main/java/iw2016_ett/email/NotificationServiceImpl.java
@@ -1,16 +1,29 @@
 package iw2016_ett.email;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
-
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+ 
+/**
+ * @author Crunchify.com
+ * 
+ */
+@Controller
+@Service("crunchifyEmail")
 public class NotificationServiceImpl {
-
-    @Autowired
-    private transient MailSender mailTemplate;
-
-    public void sendMessage(String mailTo, String message) {
-        org.springframework.mail.SimpleMailMessage mailMessage = new org.springframework.mail.SimpleMailMessage();
-        mailMessage.setTo(mailTo);
-        mailMessage.setText(message);
-        mailTemplate.send(mailMessage);
-    }
+	//@Autowired
+	private MailSender crunchifymail; // MailSender interface defines a strategy
+										// for sending simple mails
+ 
+	public void SendEmail(String toAddress, String fromAddress, String subject, String msgBody) {
+ 
+		SimpleMailMessage crunchifyMsg = new SimpleMailMessage();
+		crunchifyMsg.setFrom(fromAddress);
+		crunchifyMsg.setTo(toAddress);
+		crunchifyMsg.setSubject(subject);
+		crunchifyMsg.setText(msgBody);
+		crunchifymail.send(crunchifyMsg);
+	}
 }
+
--- ETT_IW/src/main/java/iw2016_ett/email/mail.java
+++ ETT_IW/src/main/java/iw2016_ett/email/mail.java
@@ -1,18 +1,27 @@
 package iw2016_ett.email;
 
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
 public class mail {
+	@SuppressWarnings("resource")
 	public static void main(String args[]) {
-		 
+ 
 		// Spring Bean file you specified in /src/main/resources folder
-		
+		String crunchifyConfFile = "crunchify-bean.xml";
+		ConfigurableApplicationContext context = new ClassPathXmlApplicationContext(crunchifyConfFile);
+ 
+		// @Service("crunchifyEmail") <-- same annotation you specified in CrunchifyEmailAPI.java
+		NotificationServiceImpl mail = (NotificationServiceImpl) context.getBean("crunchifyEmail");
 		String toAddr = "juangarpe94@gmail.com";
+		String fromAddr = "ett.iw2016@gmail.com";
  
 		// email subject
 		String subject = "Hey.. This email sent by Crunchify's Spring MVC Tutorial";
  
 		// email body
 		String body = "There you go.. You got an email.. Let's understand details on how Spring MVC works -- By Crunchify Admin";
-		NotificationServiceImpl m = new NotificationServiceImpl();
-		m.sendMessage(toAddr, body);
+		mail.SendEmail(toAddr, fromAddr, subject, body);
 	}
 }
